d8511a79062f9355cdd55b9c423ce81cbdab25ed
abraun <alb14d@my.fsu.edu>
2015-10-08 02:17:07 +0000

    Lesson 5 Assignment- first commit

---
+++ Lesson5_Assignment_Part1/seminole.js
@@ -0,0 +1,10 @@
+/**
+* Created with Lesson5_Assignment_Part1.
+* User: abraun
+* Date: 2015-10-08
+* Time: 02:14 AM
+* To change this template use Tools | Templates.
+*/
+define(function() {
+return {};
+});
